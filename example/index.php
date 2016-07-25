691662620002ac0e1ce2829cce0c00c1510ff92f
Mahmoud Eskandari <mahmoud-eskandari@users.noreply.github.com>
2016-07-25 15:09:38 +0430

    Example Code

---
+++ example/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *    Author : Mahmoud Eskandari
+ *    Email  : info@webafrooz.com
+ *    Website: http://webafrooz.com
+*/
+  include __DIR__.'../IranSystemConvertor.php';
+  use webafrooz\IranSystem;
+  
+  //TO Iran System Example
+  file_put_contents("IranSystem.txt",IranSystem::ToIranSystem(file_get_contents('UTF-8.txt')));
+  
+  //After Encode
+  sleep(1);
+  
+  //TO Utf-8 Example
+  echo IranSystem::FromIranSystem(file_get_contents('IranSystem.txt'));
+?>
